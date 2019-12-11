e8cb54a2a7ca2c96bc40346d7ac999b5c1ca5bd3
DavidSilvaPaiva <david654321123456@gmail.com>
2019-12-11 17:05:30 -0300

    version 1.2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-    
+function onlynumber(evt) {
+   let theEvent = evt || window.event;
+   let key = theEvent.keyCode || theEvent.which;
+   key = String.fromCharCode(key);
+   //var regex = /^[0-9.,]+$/;
+   let regex = /^[0-9.]+$/;
+   if (!regex.test(key)) {
+      theEvent.returnValue = false;
+      if (theEvent.preventDefault) theEvent.preventDefault();
+   }
+}
